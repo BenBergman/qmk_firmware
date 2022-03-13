4597a21179ff8d4d8d315fe030d21fc1742fcd84
Benjamin Bergman <ben@benbergman.ca>
2022-03-13 10:55:13 -0500

    Initial working version of pinball controller

---
+++ keyboards/pinball_controller/keymaps/default/keymap.c
@@ -0,0 +1,25 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_4x4(
+        KC_LEFT_SHIFT,
+        KC_RIGHT_SHIFT,
+        KC_LEFT_CTRL,
+        KC_RIGHT_CTRL,
+        KC_ESCAPE,
+        KC_1,
+        KC_3,
+        KC_ENTER
+    )
+};
